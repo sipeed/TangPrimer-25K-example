05a4f9e28b75843d231352690d3b0709046973bf
D <108863867+DouerGan@users.noreply.github.com>
2023-10-28 20:17:45 +0800

    Update top_demo.v

--- pmod_ledButtonNixietube/src/top_demo.v
+++ pmod_ledButtonNixietube/src/top_demo.v
@@ -1,4 +1,3 @@
-/*顶部模块*/
 module top_demo (
     input           i_clk           ,
     input           i_rst           ,
@@ -15,37 +14,37 @@ driver_Button button0(
     .i_clk           (i_clk      ),
     .i_rst           (i_rst      ),
     .i_button        (i_button[0]),
-    .o_pluse         (w_button[0])  //按键0模块
+    .o_pluse         (w_button[0])
 );
 driver_Button button1(
     .i_clk           (i_clk      ),
     .i_rst           (i_rst      ),
     .i_button        (i_button[1]),
-    .o_pluse         (w_button[1])  //按键1模块
+    .o_pluse         (w_button[1])
 );
 driver_Button button2(
     .i_clk           (i_clk      ),
     .i_rst           (i_rst      ),
     .i_button        (i_button[2]),
-    .o_pluse         (w_button[2])  //按键2模块
+    .o_pluse         (w_button[2])
 );
 driver_Button button3(
     .i_clk           (i_clk      ),
     .i_rst           (i_rst      ),
     .i_button        (i_button[3]),
-    .o_pluse         (w_button[3])  //按键3模块
+    .o_pluse         (w_button[3])
 );
 driver_NixieTube nixietube(
     .i_clk           (i_clk      ),
     .i_rst           (i_rst      ),
     .i_add           (w_button   ),
     .o_nixieTube     (o_nixieTube),
-    .o_sel           (o_sel      )  //数码管模块
+    .o_sel           (o_sel      )
 );
 driver_Led led(
     .i_clk           (i_clk      ),
     .i_rst           (i_rst      ),
     .i_switch        (i_switch   ),
-    .o_led           (o_led      )  //Led模块
+    .o_led           (o_led      )
 );
 endmodule
